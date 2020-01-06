aba00cc3c23d06ba1076af756edded28acfb639c
Jay El-Kaake <najibkaake@gmail.com>
2020-01-06 11:48:05 -0500

    Fix time formatting

--- lib/sidekiq/heartbeat_monitor/util.rb
+++ lib/sidekiq/heartbeat_monitor/util.rb
@@ -3,16 +3,29 @@ module Sidekiq
   module HeartbeatMonitor
     module Util
 
-      def format_time_str(sec_backed_up)
-        hours = (sec_backed_up - (sec_backed_up % 3600)) / 3600
-        minutes = (sec_backed_up - (sec_backed_up % 60)) / 60
-        seconds = sec_backed_up % 60
+      ##
+      # Nicely formats a seconds string.
+      # Example 1: 100.seconds => "1 min 40 sec"
+      # Example 2: 13.hours => "13 hr"
+      # 
+      # @param total_seconds [String]  Total number of seconds to format nicely.
+      # @return [String] A string representation of the time.
+      def format_time_str(total_seconds)
+        remaining_sec = total_seconds
 
-        nice_backed_up_str = "#{seconds} sec"
-        nice_backed_up_str = "#{minutes} min #{nice_backed_up_str}" if minutes > 0
+        hours = (remaining_sec - (remaining_sec % 3600)) / 3600
+        remaining_sec -= hours * 3600
+
+        minutes = (remaining_sec - (remaining_sec % 60)) / 60
+        remaining_sec -= minutes * 60
+
+        seconds = remaining_sec
+
+        nice_backed_up_str = "#{seconds} sec" if seconds > 0 || (minutes < 1 && hours < 1)
+        nice_backed_up_str = "#{minutes} min #{nice_backed_up_str}" if minutes > 0 || (seconds > 0 && hours > 0)
         nice_backed_up_str = "#{hours} hr #{nice_backed_up_str}" if hours > 0
 
-        nice_backed_up_str
+        nice_backed_up_str.strip
       end
 
     end
--- lib/sidekiq/heartbeat_monitor/version.rb
+++ lib/sidekiq/heartbeat_monitor/version.rb
@@ -1,5 +1,5 @@
 module Sidekiq
     module HeartbeatMonitor
-        VERSION = '1.0.3.0'
+        VERSION = '1.0.3.1'
     end
 end
